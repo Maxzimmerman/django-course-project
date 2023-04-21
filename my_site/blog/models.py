71643b3ed567c82a9f7bb1b9bf87904b598c27dd
Maxzimmerman <115488694+Maxzimmerman@users.noreply.github.com>
2023-04-21 16:38:37 +0200

    added images to index.hgml

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+
+class Author(models.Model):
+    # ToDo Emailfield
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)
+    excerpt = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    # ToDo Imagename
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.title} ({self.date})"
