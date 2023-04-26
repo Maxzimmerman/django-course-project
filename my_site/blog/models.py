c1956d8ef8db6b56608c21c2177b06b80de26575
Maxzimmerman <115488694+Maxzimmerman@users.noreply.github.com>
2023-04-26 16:05:45 +0200

    added images to index.hgml

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -4,6 +4,7 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+
 class Tag(models.Model):
     caption = models.CharField(max_length=10)
 
@@ -24,7 +25,7 @@ class Post(models.Model):
     title = models.CharField(max_length=20)
     excerpt = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
-    image_name = models.CharField(max_length=100, default=False)
+    image = models.ImageField(upload_to="posts", null=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
@@ -32,3 +33,11 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.date})"
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=300)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,26 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Author, Tag, Post
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from .forms import CommentForm
 
 
 # Create your views here.
 
-def starting_page(request):
-    posts_data = Post.objects.all()
-    return render(request, "blog/index.html", {
-        "posts": posts_data
-    })
+
+class StartingPage(ListView):
+    model = Post
+    template_name = "blog/index.html"
 
 
-def posts(request):
-    posts_all = Post.objects.all()
-    return render(request, "blog/all-posts.html", {
-        "all_posts": posts_all
-    })
+class Posts(ListView):
+    model = Post
+    template_name = "blog/all-posts.html"
 
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+class PostDetail(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = self.object.tags.all()
+        context['comment_form'] = CommentForm()
+        return context
